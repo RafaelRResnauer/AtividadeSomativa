d0ae842c60d9bcbe3c8a5dd0d3b9de00a662fbb2
RafaelRResnauer <resnauer.rafael@gmail.com>
2021-05-15 15:02:19 -0300

    Funções de bits adicionadas

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -116,14 +116,47 @@ public class Main {
         }
 
     }
+    public static byte[] floatToByteArray(float value) {
+        int intBits =  Float.floatToIntBits(value);
+        return new byte[] {
+                (byte) (intBits >> 24), (byte) (intBits >> 16), (byte) (intBits >> 8), (byte) (intBits) };
+    }
+
+    public static float byteArrayToFloat(byte[] bytes) {
+        int intBits =
+                bytes[0] << 24 | (bytes[1] & 0xFF) << 16 | (bytes[2] & 0xFF) << 8 | (bytes[3] & 0xFF);
+        return Float.intBitsToFloat(intBits);
+    }
+
+    private static byte[] intToBytes(final int data) {
+        return new byte[] {
+                (byte)((data >> 24) & 0xff),
+                (byte)((data >> 16) & 0xff),
+                (byte)((data >> 8) & 0xff),
+                (byte)((data >> 0) & 0xff),
+        };
+    }
+
+    private int byteArrayToInt(byte[] data) {
+        if (data == null || data.length != 4) return 0x0;
+        // ----------
+        return (int)( // NOTE: type cast not necessary for int
+                (0xff & data[0]) << 24  |
+                        (0xff & data[1]) << 16  |
+                        (0xff & data[2]) << 8   |
+                        (0xff & data[3]) << 0
+        );
+    }
 
     public static void main(String[] args) {
         //soma
-        addFunction();
+        //addFunction();
         //fatorial
-        factorial();
+        //factorial();
         //fibonacci
-        fib();
+        //fib();
+
+
 
     }
 }
